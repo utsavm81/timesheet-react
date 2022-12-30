0caee08ebfdae6f09c9dac4dace45dea029c119d
utsav modi <utsavm.itpath@gmail.com>
2022-12-30 18:54:02 +0530

    added

--- src/App.js
+++ src/App.js
@@ -415,7 +415,7 @@ function App() {
             <div
               className='mainSec'
             >
-              <h2>Enter your attendance list</h2>
+              <h2>Enter your attendance list 5555</h2>
               <textarea id="fname" name="firstname" placeholder="Paste your attendance list here...." />
               <input type="submit" onClick={calculateData} value="Calculate Time" />
             </div>
